7b89b017eaafaeac548ab1a6b1d856e2d4265eac
jilly99 <97797058+jilly99@users.noreply.github.com>
2022-06-03 11:01:09 -0400

    Add files via upload

---
+++ launchcalc.m
@@ -0,0 +1,22 @@
+function delta_v = launchcalc(n,u_eq,R) 
+
+%Inputs
+%n = number of serial stages
+%u_eq = Isp * ge, if parallel, calculate an equivalent Isp by summing
+       %thrusts of each engine and dividing by sum of their mass flow rates
+%R = M0/Mb; %mass ratio, where M0 = Ml (payload mass) + Mp (propellant
+     %mass) + Ms (structural mass), Mb = Ml + Ms
+
+%Initialize
+delta_v = 0;
+
+%Calculate delta v
+if n == 1 %single-stage
+    delta_v = u_eq*log(R); %(m/s)
+else %multi-stage
+        for i=1:n
+            delta_v = delta_v + u_eq(i) + log(R(i));
+        end
+end
+
+end
