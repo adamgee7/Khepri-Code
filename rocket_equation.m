5076bf209ad63ff58ee4c611e213c02b5c63acae
adamgee7 <105932254+adamgee7@users.noreply.github.com>
2022-06-07 10:52:45 -0400

    Add files via upload

--- rocket_equation.m
+++ rocket_equation.m
@@ -1,4 +1,11 @@
 function m_fuel = rocket_equation(delta_v,mf,Isp)
+% PLEASE READ INPUTS BEFORE USE
+
+%Inputs: Delta-v in m/s, final/dry mass of spacecraft (after fuel has 
+%been burned) in kg, Isp in s is specific impulse to find amount
+%of fuel
+%Outputs: Mass of fuel in kg
+
 g0 = 9.81;
 m0 = mf*exp(delta_v/(Isp*g0));
 m_fuel = m0-mf;
